1af61258e46c30ebd4a7e643383f87456f57a831
Tawfik Yasser <tawfekyassertawfek@gmail.com>
2021-02-09 06:43:46 +0200

    Update problem-1.cpp

--- problem-1.cpp
+++ problem-1.cpp
@@ -18,13 +18,21 @@ int minSeekTime(vector<int> vList,int head,int list_size){
 int main()
 {
     int list_size = 3 ;
-
     vector<int> vList = {100,50,190};
 
-    //sort(vList.begin(), vList.end(), greater<int>());
+    //For testing
+    //vector<int> vList = {176,79,34,60,92,11,41,114}; => size = 8, head = 140
+    //vector<int> vList = {34,15,62,21,90}; => size = 5, head = 44
 
+    sort(vList.begin(), vList.end(), less<int>());
     int head = 140;
-
+    int diff1 = abs(head - vList[0]);
+    sort(vList.begin(), vList.end(), greater<int>());
+    int diff2 = abs(head - vList[0]);
+    if(diff1 < diff2)
+         sort(vList.begin(), vList.end(), less<int>());
+    else if(diff1 > diff2)
+        sort(vList.begin(), vList.end(), greater<int>());
     cout<<"Minimal Seek Time = "<<minSeekTime(vList,head,list_size)<<endl;
 
     return 0;
